dd3054e6fb50fef22e79f8847384281f5562aaa8
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-09-28 19:43:31 +0300

    Add & configure: - Configure Django/settings.py - Configure Django/urls.py - Add & configure generate_teachers/urls.py - Configure generate_teachers/views.py

--- Django/settings.py
+++ Django/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'generate_teachers.apps.GenerateTeachersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- generate_teachers/views.py
+++ generate_teachers/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from faker import Faker
 
-# Create your views here.
+
+fake = Faker()
+
+
+def index(request):
+    return HttpResponse("<h1>Homework-5 index page</h1>")
+
+
+def generate_teachers(request):
+    return HttpResponse("<h1>`generate_teachers` page</h1>")
