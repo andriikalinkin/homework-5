9df54c96ea84d62e4252bd447181466bc680409f
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-10-02 12:50:07 +0200

    Delete blank lines. Change the return

--- generate_teachers/views.py
+++ generate_teachers/views.py
@@ -7,10 +7,8 @@ fake = Faker()
 
 
 def index(request):
-    return HttpResponse("<h1>Homework-5 index page</h1>")
+    return HttpResponse("<h1>Homework-5 index page. CHANGES</h1>")
 
 
 def generate_teachers(request):
     return HttpResponse("<h1>`generate_teachers` page</h1>")
-
-
