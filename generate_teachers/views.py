75c6b542b4127c3a7aee94dab515b5bd61e3483b
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-10-02 11:51:37 +0200

    Commit #5

--- generate_teachers/views.py
+++ generate_teachers/views.py
@@ -7,7 +7,7 @@ fake = Faker()
 
 
 def index(request):
-    return HttpResponse("<h1>Homework-5 index page</h1>")
+    return HttpResponse("<h1>Homework-5 index page. HERE</h1>")
 
 
 def generate_teachers(request):
