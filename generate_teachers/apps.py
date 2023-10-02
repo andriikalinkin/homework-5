fa01ef69979f42e132fe998e38fd3c5a1231dd97
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-10-02 11:47:34 +0200

    Commit #3

--- generate_teachers/apps.py
+++ generate_teachers/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class GenerateTeachersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'generate_teachers'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "generate_teachers"
